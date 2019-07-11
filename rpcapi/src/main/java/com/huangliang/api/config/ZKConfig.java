dd0b8c5a0adfd919cf1a60d4cc877cd89b34d80d
huangliang <hl7226261@2980.com>
2019-07-11 21:02:42 +0800

    接入zookeeper配置

---
+++ rpcapi/src/main/java/com/huangliang/api/config/ZKConfig.java
@@ -0,0 +1,6 @@
+package com.huangliang.api.config;
+
+public interface ZKConfig {
+    public static String rootPath = "/rpcserverzk";
+    public static String zkAddress = "192.168.99.100:2181";
+}
